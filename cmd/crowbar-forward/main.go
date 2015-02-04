7582aeace13c82be6bd9d7b1f31d9f872aa82090
Sergiusz 'q3k' Bazański <q3k@q3k.org>
2015-02-04 13:32:46 +0100

    Extend client to full functionality

--- cmd/crowbar-forward/main.go
+++ cmd/crowbar-forward/main.go
@@ -5,32 +5,52 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"os"
 
 	"github.com/q3k/crowbar"
+	"github.com/howeyc/gopass"
 )
 
 func main() {
-	local := flag.String("local", "127.0.0.1:22", "Local address to bind to.")
+	local := flag.String("local", "127.0.0.1:1122", "Local address to bind to, or - for stdin/out")
 	remote := flag.String("remote", "HOST:PORT", "Remote address to establish tunnel to.")
-	server := flag.String("server", "http://example.com:80/", "Crowbar server to use.")
+	server := flag.String("server", "http://127.0.0.1:8080/", "Crowbar server to use.")
+	username := flag.String("username", "", "Username to use.")
+	password := flag.String("password", "", "Password to use, or empty for interactive getpass.")
 	flag.Parse()
 
-	fmt.Println(*local, *remote, *server)
-
-	localListen, err := net.Listen("tcp", *local)
-	if err != nil {
-		panic(err)
+	if *username == "" {
+		fmt.Fprintf(os.Stderr, "Username must be given.\n")
+		return
 	}
-	for {
-		localConn, err := localListen.Accept()
+	if *password == "" {
+		fmt.Fprintf(os.Stderr, "Password: ")
+		*password = string(gopass.GetPasswd())
+	}
+
+	if *local == "-" {
+		remoteConn, err := crowbar.Connect(*server, *username, *password, *remote)
 		if err != nil {
 			panic(err)
 		}
-		remoteConn, err := crowbar.Connect(*server, "q3k", "dupa.8", *remote)
+		go io.Copy(remoteConn, os.Stdin)
+		io.Copy(os.Stdout, remoteConn)
+	} else {
+		localListen, err := net.Listen("tcp", *local)
 		if err != nil {
 			panic(err)
 		}
-		go io.Copy(localConn, remoteConn)
-		io.Copy(remoteConn, localConn)
+		for {
+			localConn, err := localListen.Accept()
+			if err != nil {
+				panic(err)
+			}
+			remoteConn, err := crowbar.Connect(*server, *username, *password, *remote)
+			if err != nil {
+				panic(err)
+			}
+			go io.Copy(localConn, remoteConn)
+			io.Copy(remoteConn, localConn)
+		}
 	}
 }
